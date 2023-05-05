d7d4889a7d32358406a8b81d0d68d2cd02ac4e1d
Ameer Salman <92325741+AmeerSalman2627@users.noreply.github.com>
2023-05-06 02:53:15 +0530

    Destination Continent Page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Components/Account/Login';
+import Destination from './Components/Destination/Destination';
+import CommonDashboard from './Components/Dashboard/CommonDashboard';
 
 function App() {
   return (
-    <div className='app_container'>
-      <Login></Login>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/destination" element={<Destination />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- ui/src/Components/Account/PersonalLogin.js
+++ ui/src/Components/Account/PersonalLogin.js
@@ -1,6 +1,36 @@
 import { Row } from 'react-bootstrap';
+<<<<<<< Updated upstream
 
 const PersonalLogin = () => {
+=======
+import $ from 'jquery';
+import { useForm } from "react-hook-form";
+import { FaArrowLeft } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const PersonalLogin = () => {
+
+    useEffect(() => {
+        $('.form_error').hide();
+    },[])
+    const [showpwd, setShowPwd] = useState(false);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const login = (data) => {
+        console.log(data);
+        if(data !== '' || data !== null){
+            $('.Personal_Login_Form').attr('hidden', true);
+            $('.after_login_section').attr('hidden', false);
+            $('.nav-justified').attr('hidden', true);
+        }
+        
+    }
+    const backToLogin = () => {
+        $('.Personal_Login_Form').attr('hidden', false);
+        $('.after_login_section').attr('hidden', true);
+        $('.nav-justified').attr('hidden', false);
+    }
+>>>>>>> Stashed changes
     return (
         <Row className='personal_login_section'>
             <form className='Personal_Login_Form'>
@@ -18,8 +48,30 @@ const PersonalLogin = () => {
                 <p className='fgt_pwd'>Forgot Password?</p>
                 <button className="btn submit_btn" type="submit">Sign in</button>
             </form>
+<<<<<<< Updated upstream
             <div className='tnc_section' style={{marginTop:'10px'}}>
                 <p style={{textAlign:'center'}}>New to Locofest? Create an account</p>
+=======
+            <Row className='after_login_section' hidden>
+                <button className="btn otp_back" type="submit" onClick={backToLogin}><FaArrowLeft />Back</button>
+                <div className='user_img mt-2'>
+                    <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar" />
+                    <p className='mt-2'>Welcome Back, UserName!</p>
+                </div>
+                <div className="otp-section">
+                    <p>Please enter the sent OTP</p>
+                    <div className="input-group">
+                        <input type="text" id="otp" name="otp" required />
+                        <label htmlFor="otp">One Time Password</label>
+                    </div>
+                    <Link to="/destination" className='verify_otp_link'>
+                        <button className="btn otp_verify" type="submit">Verify OTP</button>
+                    </Link>
+                </div>
+            </Row>
+            <div className='tnc_section' style={{ marginTop: '10px' }}>
+                <p style={{ textAlign: 'center' }}>New to Locofest? Create an account</p>
+>>>>>>> Stashed changes
                 <p>By proceeding, you agree to <span className="privacy">Locofest's  Privacy Policy</span>, <span className='agreement'>User Agreement</span> and <span className="tnc">T&Cs</span></p>
             </div>
         </Row>
