dd6efb69e91a7353475dcc3396fe024a16b241db
Ameer Salman <92325741+AmeerSalman2627@users.noreply.github.com>
2023-05-06 23:17:25 +0530

    Destination Page Created
    Changes made in personallogin.js and destination page

--- ui/src/Components/Account/PersonalLogin.js
+++ ui/src/Components/Account/PersonalLogin.js
@@ -1,23 +1,9 @@
 import { Row } from 'react-bootstrap';
-
-
-const PersonalLogin = () => {
-
-import $ from 'jquery';
-import { useForm } from "react-hook-form";
-import { FaArrowLeft } from 'react-icons/fa';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-const PersonalLogin = () => {
-
-    useEffect(() => {
-        $('.form_error').hide();
-    },[])
 import $ from 'jquery';
 import { useForm } from "react-hook-form";
 import { FaArrowLeft } from 'react-icons/fa';
 import { useState } from 'react';
+import { Link } from "react-router-dom";
 
 const PersonalLogin = () => {
 
@@ -67,9 +53,6 @@ const PersonalLogin = () => {
                 <button className="btn submit_btn" type="submit" >Sign in</button>
             </form>
 
-            <div className='tnc_section' style={{marginTop:'10px'}}>
-                <p style={{textAlign:'center'}}>New to Locofest? Create an account</p>
-
             <Row className='after_login_section' hidden>
                 <button className="btn otp_back" type="submit" onClick={backToLogin}><FaArrowLeft />Back</button>
                 <div className='user_img mt-2'>
@@ -81,20 +64,17 @@ const PersonalLogin = () => {
                     <div className="input-group">
                         <input type="text" id="otp" name="otp" required />
                         <label htmlFor="otp">One Time Password</label>
-                    </div>
+                    </div> 
 
                     <Link to="/destination" className='verify_otp_link'>
                         <button className="btn otp_verify" type="submit">Verify OTP</button>
                     </Link>
 
-                    <button className="btn otp_verify" type="submit">Verify OTP</button>
 
                 </div>
             </Row>
             <div className='tnc_section' style={{ marginTop: '10px' }}>
                 <p style={{ textAlign: 'center' }}>New to Locofest? Create an account</p>
-
-
                 <p>By proceeding, you agree to <span className="privacy">Locofest's  Privacy Policy</span>, <span className='agreement'>User Agreement</span> and <span className="tnc">T&Cs</span></p>
             </div>
         </Row>
